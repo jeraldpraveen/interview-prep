edd08af265879c55a701eb0f74a3b7be5fec1183
jeraldpraveen <jerald.vit@gmail.com>
2022-07-03 23:21:52 +0530

    26. Currying

---
+++ 13-currying/currying.js
@@ -0,0 +1,25 @@
+// Normal Function Syntax
+// function getProduct(num1, num2) {
+//   return num1 * num2;
+// }
+
+function getProduct(num1) {
+  return function (num2) {
+    return num1 * num2;
+  };
+}
+getProduct(10)(20);
+
+// Normal Function Syntax
+// function getTravelTime(distance, speed) {
+//   return distance / speed;
+// }
+
+function getTravelTime(distance) {
+  return function (speed) {
+    return distance / speed;
+  };
+}
+const travelTimeBosNyc = getTravelTime(400);
+console.log(travelTimeBosNyc(100));
+console.log(travelTimeBosNyc(200));
