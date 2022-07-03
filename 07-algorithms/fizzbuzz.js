83c40954928984dfdc8a8b23f94259bb6680c0a8
jeraldpraveen <jerald.vit@gmail.com>
2022-07-03 11:36:23 +0530

    7. FizzBuzz Algorithm

---
+++ 07-algorithms/fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(num) {
+  let index = 1;
+  while (index <= num) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (index % 3 === 0) {
+      console.log("Fizz");
+    } else if (index % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(index);
+    }
+    index++;
+  }
+}
+
+fizzbuzz(20);
