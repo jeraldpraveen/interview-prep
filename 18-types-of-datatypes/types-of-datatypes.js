b66ca8f779ccd419ce80a8238e46040234882e0e
jeraldpraveen <jerald.vit@gmail.com>
2022-07-04 00:02:40 +0530

    31. Types Of Data Types

---
+++ 18-types-of-datatypes/types-of-datatypes.js
@@ -0,0 +1,9 @@
+console.log(typeof 1);
+console.log(typeof "Jerald");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+console.log(Array.isArray([]));
+console.log([] instanceof Array);
